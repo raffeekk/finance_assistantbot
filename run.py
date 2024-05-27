ee67fb5ec2bd8c559e95f6aed8c88204f719878b
raffeekk <idmitryfrl@gmail.com>
2024-05-27 19:04:06 +0300

    dotenv_token

--- run.py
+++ run.py
@@ -3,8 +3,12 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils import executor
+from dotenv import load_dotenv
+import os
 
-API_TOKEN = '7310816247:AAGdCAoCim82ca8AfLxhpbAgUA8Bb1k5cuc'
+# Load environment variables from .env file
+load_dotenv()
+API_TOKEN = os.getenv('API_TOKEN')
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -21,23 +25,23 @@ keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True).add(button1).add(but
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Welcome! This is the /start command. Use the buttons below to interact with the bot.", reply_markup=keyboard_markup)
+    await message.reply("Welcome! To interact with the bot, use the buttons below.", reply_markup=keyboard_markup)
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
     help_text = (
-        "This is the /help command. Here you can describe how to use your bot.\n"
-        "You can add more detailed instructions or information as needed."
+        "This is the /help command. Information about the bot will be provided here..\n"
+        "And also here the user learns about the functionality of the bot."
     )
     await message.reply(help_text)
 
 @dp.message_handler(lambda message: message.text == "Button 1")
 async def handle_button1(message: types.Message):
-    await message.reply("You pressed Button 1!")
+    await message.reply("Вы нажали Button 1!")
 
 @dp.message_handler(lambda message: message.text == "Button 2")
 async def handle_button2(message: types.Message):
-    await message.reply("You pressed Button 2!")
+    await message.reply("Вы нажали Button 2!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
